6e7bb79718c99264ad55b079a79c06e4af6702c6
giangnt2011 <125941500+giangnt2011@users.noreply.github.com>
2023-02-23 17:28:58 +0700

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform rotator;
+
+    private Rigidbody cubeRb;
+    public float speed = 5.0f;
+
+    private Vector2 input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeRb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (GetInput("Horizontal", "Vertical"))
+        {
+            MovePlayer();
+        }
+        if (GetInput("MouseX", "MouseY"))
+        {
+            RotateTurret();
+        }
+    }
+
+    bool GetInput(string horizontal, string vertical)
+    {
+        input.x = SimpleInput.GetAxisRaw(horizontal) * speed;
+        input.y = SimpleInput.GetAxisRaw(vertical) * speed;
+        return (Mathf.Abs(input.x) > 1f) || (Mathf.Abs(input.y) > 1f);
+    }
+
+    void MovePlayer()
+    {
+        cubeRb.velocity = Vector3.Normalize(new Vector3(input.x, 0,input.y)) * speed;
+    }
+
+    void RotateTurret()
+    {
+        rotator.eulerAngles = new Vector3(0, Mathf.Atan2(input.x, input.y) * 180 / Mathf.PI, 0);
+    }
+
+  
+
+}
